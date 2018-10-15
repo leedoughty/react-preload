08c4dab11818bfa6bd2c596e0777f5d2b82ab862
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-10-15 11:03:27 +0100

    Add styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  color: white;
+  background-color: black;
+  width: 100%;
+  height: 100%;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em
+  font-family: "Helvetica";
+`
 
 class Layout extends Component {
   render() {
     return (
-      <p>Preload</p>
+      <Wrapper>
+        <Title>
+          <p>Preload</p>
+        </Title>
+      </Wrapper>
     )
   }
 }
